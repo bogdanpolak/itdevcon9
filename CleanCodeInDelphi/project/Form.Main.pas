80d86fe83447bf63f38a2774e0bc323f0c616224
bogdanpolak <bogdan.polak@bsc.com.pl>
2018-10-01 16:58:04 +0200

    Changed MainForm in demo project

--- CleanCodeInDelphi/project/Form.Main.pas
+++ CleanCodeInDelphi/project/Form.Main.pas
@@ -4,10 +4,18 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.ExtCtrls, ChromeTabs;
 
 type
   TForm1 = class(TForm)
+    GroupBox1: TGroupBox;
+    lbTitleFilesToAdd: TLabel;
+    Splitter1: TSplitter;
+    lbTitleFilesToRemove: TLabel;
+    lbxFilesToAdd: TListBox;
+    lbxFilesToRemove: TListBox;
+    ChromeTabs1: TChromeTabs;
+    pnMain: TPanel;
   private
     { Private declarations }
   public
